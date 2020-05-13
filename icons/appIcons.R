b562caf6ab0534d19d1e92be20b2428055450d08
Ashley Noel Hinton <ashley@canadia.co.nz>
2020-05-13 16:36:50 +1200

    produce app icons for iOS

---
+++ icons/appIcons.R
@@ -0,0 +1,20 @@
+#! /usr/bin/env Rscript
+
+library(whisker)
+
+source = "pizzaIcon.png"
+
+sizes = c(40, 58, 60, 80, 87, 120, 152, 167, 180, 1024)
+
+output = file.path("appIcons", paste0("pizzaIcon-", sizes, ".png"))
+
+template = "{{source}} -scale {{size}} {{output}}"
+
+
+for (i in seq_along(sizes)) {
+    system2(command = "convert",
+            args = whisker.render(template = template,
+               data = list(source = source,
+                           size = sizes[i],
+                           output = output[i])))
+}
